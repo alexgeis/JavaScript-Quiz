4ea3a985fe099cbc9cbdf5e187008c53a4f9a419
Alex Geis <siegxela@gmail.com>
2023-01-08 17:31:45 -0700

    update data - add css question and import

--- quiz-app/data/index.ts
+++ quiz-app/data/index.ts
@@ -1,3 +1,5 @@
 import { javascriptQs } from "./javascript";
+import { awsQs } from "./aws";
+import { cssQs } from "./css";
 
-export const questions = [...javascriptQs];
+export const questions = [...javascriptQs, ...awsQs, ...cssQs];
