5428da049c3ae827455ec38b5b089caf6d5e80f0
Alex Geis <siegxela@gmail.com>
2023-02-05 12:17:18 -0700

    add hyperlinks to all quiz topics to link to respective pages

--- quiz-app/components/QuizTopics.tsx
+++ quiz-app/components/QuizTopics.tsx
@@ -1,11 +1,20 @@
 import styles from "./QuizTopics.module.css";
+import Link from "next/link";
 
 const data: string[] = ["aws", "JavaScript", "http", "Python", "Java"];
+
 function QuizTopics() {
 	return (
 		<main className={styles.wrapper}>
 			{data.map((data, index) => {
-				return <p key={index}>{data}</p>;
+				return (
+					<Link
+						key={index}
+						href={`/${data}`}
+					>
+						<p className={styles.topicName}>{data}</p>
+					</Link>
+				);
 			})}
 		</main>
 	);
