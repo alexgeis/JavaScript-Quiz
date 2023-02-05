7cc63c5a48d38f464858a610bfe79dd97fae7d31
Alex Geis <siegxela@gmail.com>
2023-02-05 16:36:14 -0700

    add useEffect to Layout to grab page URL and replace topic state with value

--- quiz-app/components/Layout.tsx
+++ quiz-app/components/Layout.tsx
@@ -1,20 +1,53 @@
 "use client";
-import { useState } from "react";
+import { ReactNode, useState, useEffect } from "react";
+import Head from "next/head";
+import { useRouter } from "next/router";
 import styles from "Layout.module.css";
 import { NavBar } from "./NavBar";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 
-export default function Layout({ children }: any) {
+type LayoutProps = {
+	children: React.ReactNode;
+};
+
+export default function Layout({ children }: LayoutProps) {
 	const [topic, setTopic] = useState<string>(
 		"Take a quiz - test your knowledge"
 	);
+	console.log("state", topic);
 	const [subText, setSubText] = useState<string>(
 		"Questions are sorted by category"
 	);
 
+	const router = useRouter();
+	const currentUrlPath = router.pathname.replace(/\//, "");
+
+	useEffect(() => {
+		if (currentUrlPath !== "") setTopic(currentUrlPath);
+		else {
+			setTopic("Take a quiz - test your knowledge");
+			setSubText("Questions are sorted by category");
+		}
+	}, [currentUrlPath]);
+
 	return (
 		<>
+			<Head>
+				<title>DevTrain</title>
+				<meta
+					name="DevTrain"
+					content="Quiz application for programmers"
+				/>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1"
+				/>
+				<link
+					rel="icon"
+					href="/favicon.ico"
+				/>
+			</Head>
 			<NavBar />
 			<Header
 				mainText={topic}
