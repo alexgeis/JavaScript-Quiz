3cca81132e687728721e1fe4aed0dc35024a82c8
Alex Geis <siegxela@gmail.com>
2023-02-18 09:17:50 -0700

    add darkmode context provider to _app.tsx (wrapped entire app in context)

--- quiz-app/pages/_app.tsx
+++ quiz-app/pages/_app.tsx
@@ -2,13 +2,16 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import { TopicProvider } from "../context/TopicContext";
+import { DarkModeContextProvider } from "../context/ThemeContext";
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<TopicProvider>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
+			<DarkModeContextProvider>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</DarkModeContextProvider>
 		</TopicProvider>
 	);
 }
